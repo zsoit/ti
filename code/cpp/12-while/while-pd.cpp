9bf3285058aaeaffc99a4874622cf7938d96291a
zsoit <wsparcie.exe@gmail.com>
2020-11-08 18:27:24 +0100

    12.while-for

--- code/cpp/12-while/while-pd.cpp
+++ code/cpp/12-while/while-pd.cpp
@@ -1,10 +1,10 @@
-//za pomoca petli for i while prosze napisac program ktory bedzie pobieral znka z klawiatury i bedzie wypisal okreslona liczbe razy przez uzytkownika 
+//za pomoca petli for i while prosze napisac program ktory bedzie pobieral znaka z klawiatury i bedzie wypisal okreslona liczbe razy przez uzytkownika 
 
 #include <iostream>
 #include <cstdio>
 using namespace std;
 int main() {
- char znak; int razy, i; //zmienna char, moze pobrac kazdy pojedynczy znak/cyfre
+ char znak; int razy, i; //zmienna typu char, moze pobrac kazdy pojedynczy znak/cyfre, nie pobiera wiecej niz jeden znak/cyfre;
 
   //PETLA-WHILE
   cout<<"Podaj znak: "; cin>>znak;
