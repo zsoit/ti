bc1c9d4106a791bbb20bd910da1cca1e2771aad3
zsoit <wsparcie.exe@gmail.com>
2020-11-16 22:21:52 +0100

    15

---
+++ code/cpp/15-funkcje/pd-fun.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int suma(int a, int b){
+    return (a+b);
+}
+
+int roznica(int a, int b){
+    return (a-b);
+}
+
+int iloczyn(int a, int b){
+    return (a*b);
+}
+
+int iloraz(int a, int b){
+    return (a/b);
+}
+int main()
+{
+char znak; int wybor,i,razy,x,y;
+
+do
+{
+    cout<<"KALKULATOR - WYBIERZ DZIALANIE: \n1) SUMA \n2) ROZNICA \n3) ILOCZYN \n4) ILORAZ \n";
+    cout<<"Ktore dzialanie chcesz wybrac? ";
+    cin >> wybor;
+    switch (wybor)
+    {
+        case 1:
+        {
+            cout <<"==> wybrales DODAWANIE \n";
+            cout<<"Podaj liczbe a: "; cin>>x;
+            cout<<"Podaj liczbe b: "; cin>>y;
+            cout<<"Suma wynosi: "<<suma(x,y)<<endl;
+            break;
+        }
+        case 2:
+        {
+            cout <<"==> wybrales ODEJMOWANIE \n";
+            cout<<"Podaj liczbe a: "; cin>>x;
+            cout<<"Podaj liczbe b: "; cin>>y;
+            cout<<"Suma wynosi: "<<roznica(x,y)<<endl;
+            break;
+        }
+
+        case 3:{
+            cout <<"==> wybrales MNOZENIE \n";
+            cout<<"Podaj liczbe a: "; cin>>x;
+            cout<<"Podaj liczbe b: "; cin>>y;
+            cout<<"Suma wynosi: "<<iloczyn(x,y)<<endl;
+            break;
+        }
+
+        case 4:{
+            cout<<"==> wybrales DZIELNIE\n";
+            cout<<"Podaj liczbe a: "; cin>>x;
+            cout<<"Podaj liczbe b: "; cin>>y;
+            cout<<"Suma wynosi: "<<iloraz(x,y)<<endl;
+            break;
+        }
+        default:{
+            cout<<"Nieprawny wybór! \n";
+            break;
+        }
+    }
+    cout <<"==> Czy chcesz zakonczyc? : t/n? ";
+    cin >> znak;
+    system ("clear");
+}
+while (znak != 't' && znak == 'n');
+cout<<"==> Dziekuje, zakoczyles dzialanie programu! J.A.\n";
+getchar();
+return 0;
+} 
